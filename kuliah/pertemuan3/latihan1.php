e163626d69bc6bee25222fbd01c2bb6a10d10c78
Chalidarlh <chalidarlh@gmail.com>
2024-02-24 11:17:59 +0700

    first commit

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>Hello World!</h1>";
+?>
+
+<?php
+echo "<p>Chalida</p>";
+?>
+
+<?php
+echo 1 + 2;
+?>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,6 @@
+<?php
+$nama = "Chalida";
+$umur = 17;
+$univ = "Universitas Pasundan";
+
+echo "Hello, nama saya $nama, umur saya $umur tahun, saya kuliah di $univ";
